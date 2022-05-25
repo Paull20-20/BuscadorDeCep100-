a41efec3b4e66476e895e838a208074e04c7c8c2
Paull20-20 <paulloholiveira77@gmail.com>
2022-05-25 16:25:02 -0300

    Add project files.

---
+++ BuscadorDeCep/BuscadorDeCep/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Correios;
+
+
+namespace BuscadorDeCep
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnProcurar_Clicked(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(entCep.Text))
+            {
+                await DisplayAlert("Campo de busca vázio", "Busque novamente", "ok");
+            }
+            else
+            {
+                CorreiosApi correios = new CorreiosApi();
+                Correios.CorreiosServiceReference.enderecoERP result = correios.consultaCEP(entCep.Text);
+
+                lblCep2.Text = result.cep;
+                lblEstado.Text = result.uf;
+                lblCidade.Text = result.cidade;
+                lblBairro.Text = result.bairro;
+                lblRua.Text = result.end;
+
+            }
+
+
+        }
+    }
+}
